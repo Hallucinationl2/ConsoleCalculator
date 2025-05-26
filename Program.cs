ce51bbf03e59224c4fc63b7dc64fde640f7710d5
Radoslav <117578667+Hallucinationl2@users.noreply.github.com>
2025-05-26 21:01:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Write("Въведете първо число: ");
+            if (!double.TryParse(Console.ReadLine(), out double num1)) continue;
+
+            Console.Write("Операция (+ - * /): ");
+            string op = Console.ReadLine();
+
+            Console.Write("Въведете второ число: ");
+            if (!double.TryParse(Console.ReadLine(), out double num2)) continue;
+
+            double result = 0;
+            switch (op)
+            {
+                case "+": result = num1 + num2; break;
+                case "-": result = num1 - num2; break;
+                case "*": result = num1 * num2; break;
+                case "/": result = num2 != 0 ? num1 / num2 : double.NaN; break;
+                default: Console.WriteLine("Невалидна операция"); continue;
+            }
+
+            Console.WriteLine($"Резултат: {result}\n");
+        }
+    }
+}
